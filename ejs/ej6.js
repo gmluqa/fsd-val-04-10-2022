e5b66476c09f0d3b9d14593af4a7ee155634bdc4
gmluqa <gmluqa@protonmail.com>
2022-10-04 09:39:21 +0200

    done upto ej9

---
+++ ejs/ej6.js
@@ -0,0 +1,7 @@
+var num = prompt("Introduce un numero");
+
+const IVA = 0.21;
+
+var precioFinal = parseFloat(num) * IVA + parseFloat(num);
+
+console.log(precioFinal);
---
+++ ejs/ej7.js
@@ -0,0 +1,6 @@
+var num = 1;
+
+while (num <= 100) {
+  console.log(num);
+  num++;
+}
