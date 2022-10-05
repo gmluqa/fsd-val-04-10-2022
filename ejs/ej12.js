236b9277e6ae9a8591c37973e0ca926eec1bb42b
gmluqa <gmluqa@protonmail.com>
2022-10-05 09:18:30 +0200

    gave for loop a let

--- ejs/ej12.js
+++ ejs/ej12.js
@@ -1,7 +1,7 @@
 let pass = "password1234";
 let userInput = "";
 
-for (i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
   userInput = prompt("Enter password, " + (3 - i) + " tries left!");
   if (userInput == pass) {
     console.log("Congratulations!");
